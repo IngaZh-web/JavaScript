8e57393f128e1e0b05abe32c7e782f84d8152a70
Inga Zhelnova <detal.07@mail.ru>
2024-03-21 14:24:32 +0300

    lesson 10

--- lesson_07/task04.js
+++ lesson_07/task04.js
@@ -1,23 +1,24 @@
-'use strict'
+"use strict";
 
 const allСashbox = [
-  [12, 4500], 
-  [7, 3210], 
-  [4, 650], 
-  [3, 1250], 
-  [9, 7830], 
-  [1, 990], 
-  [6, 13900], 
-  [1, 370]
+  [12, 4500],
+  [7, 3210],
+  [4, 650],
+  [3, 1250],
+  [9, 7830],
+  [1, 990],
+  [6, 13900],
+  [1, 370],
 ];
 
+const arrSum = allСashbox.map((subArr) =>
+  subArr.flat(Infinity).reduce((subSum, item) => subSum * item)
+);
 
-	const arrSum = allСashbox.map(subArr => subArr.flat(Infinity).reduce((subSum, item) => (subSum * item)));
-
-	const average =  arrSum.reduce((acc, c) => acc + c / arrSum.length, 0);
+const getAveragePriceGoods = () => {
+  return arrSum.reduce((acc, c) => acc + c / arrSum.length, 0);
+};
+getAveragePriceGoods();
 
 console.log(arrSum);
-console.log(average);
-
-
-
+console.log(getAveragePriceGoods());
---
+++ lesson_10/task.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const cart = {
+  items: [],
+  count: 0, 
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+
+  add: function (item, price, quantity = 1) {
+    this.items.push({
+      item: item,
+      price: price,
+      quantity: quantity,
+    });
+    this.increaseCount(quantity);
+    this.calculateItemPrice();
+  },
+  calculateItemPrice() {
+    return this.items.reduce((totalSumm, item) => {
+      totalSumm += item.price * item.quantity;
+      return totalSumm;
+    }, 0);
+  },
+
+  increaseCount(count) {
+    this.count += count;
+  },
+
+  print() {
+    const str = JSON.stringify(this.items);
+    console.log("str: ", str);
+    console.log("Total price: ", this.totalPrice);
+  },
+
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
+};
+
+cart.add("салфетка", 15, 20);
+cart.add("вилка", 5, 23);
+cart.print();
+cart.clear();
+console.log(cart);
